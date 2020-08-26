2b47284a5b0b1f8783253e87b133b88b35a9c593
Shashank Gaur <gaurshashank014@gmail.com>
2020-08-26 11:58:24 +0530

    used withRouter HOC to navigate to different menu-items route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/Homepage/homepage.component';
-
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    HatsPage
+  </div>
+)
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import {withRouter} from 'react-router-dom'
 
 const MenuItem = (props) => (
-  <div className={`${props.section.size} menu-item`}>
+  <div className={`${props.section.size} menu-item`} onClick={() => props.history.push(`${props.match.url}${props.section.linkUrl}`)}>
     <div className='background-image' style={{backgroundImage: `url(${props.section.imageUrl})`}} />
     <div className='content'>
       <div className='title'>{props.section.title.toUpperCase()}</div>
@@ -10,4 +11,4 @@ const MenuItem = (props) => (
     </div>
   </div>
 )
-export default MenuItem
+export default withRouter(MenuItem)
